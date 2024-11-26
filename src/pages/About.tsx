c34862e34e5f2850f01b845636ae9ae35a581292
thegrimkiller22 <anubhav44r@gmail.com>
2024-11-26 22:05:15 +0530

    to update gallery

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -114,13 +114,27 @@ const About: React.FC = () => {
 				<div className='px-6 py-16 order-2'>
 					<h3 className='text-primary font-bold inline-flex items-center justify-start gap-4 text-lg'>
 						{' '}
-						<span className='w-8 h-0.5 bg-primary inline-block' /> What We Do
+						<span className='w-8 h-0.5 bg-primary inline-block' /> About Us
 					</h3>
-					<h2 className='text-5xl font-bold mb-4'>Vehicle Distribution Centers</h2>
+					<h2 className='text-5xl font-bold mb-4'>About The Partners</h2>
 					<p className='text-gray-500 text-lg leading-relaxed '>
-						"In addition to our core rail transportation network for finished vehicle distribution, APL Logistics VASCOR Automotive will operate a network of vehicle distribution centres and yards.Sites are strategically located adjacent
-            to the rail terminals. These Centers provide various value-added services, including :
-            
+          <p className="text-lg text-gray-700 mb-4">
+             APL Logistics provides international, integrated supply chain services in more than 73 countries, including both
+             origin and destination services such as freight consolidation, warehousing and distribution management. It uses innovative IT for maximum supply chain visibility and control.
+           </p>
+           <a href="https://www.apllogistics.com" className="text-blue-600 hover:underline">
+             Visit APL Logistics Website
+          </a>
+          <h3 className="text-2xl font-semibold text-gray-900 mt-8 mb-4">VASCOR</h3>
+           <p className="text-lg text-gray-700 mb-4">
+             VASCOR is a leading provider of logistics services to the automotive sector. Formed in 1987, the company is a joint
+            venture of Singapore-based supply chain management leader APL Logistics and FUJITRANS Corporation of Japan. Based in
+            Georgetown, Kentucky, USA, VASCOR provides a full spectrum of logistics services to major automotive manufacturers
+            and sales entities, as well as all U.S. Class One Railroads.
+           </p>
+           <a href="https://www.vascorlogistics.com" className="text-blue-600 hover:underline">
+             Visit VASCOR Website
+           </a>  
 					</p>
 				</div>
 				<div className='w-full h-full bg-red-50'>
@@ -130,6 +144,54 @@ const About: React.FC = () => {
 					/>
 				</div>
 			</div>
+      <div className='grid grid-cols-2 w-full h-dvh'>
+				<div className='px-6 py-16'>
+					<h3 className='text-primary font-bold inline-flex items-center justify-start gap-4 text-lg'>
+						{' '}
+						<span className='w-8 h-0.5 bg-primary inline-block' /> About us
+					</h3>
+					<h2 className='text-5xl font-bold mb-4'>Board of Directors</h2>
+					<p className='text-gray-500 text-lg leading-relaxed '>
+
+          <ul className="list-inside list-disc space-y-4 text-lg text-gray-700">
+           <li><strong>Thad Bedard</strong> - President, APL Logistics & Chairman - APL Logistics VASCOR Automotive Pvt. Ltd.</li>
+           <li><strong>TOMONORI KONO</strong> - Director, APL Logistics VASCOR Automotive Pvt. Ltd.</li>
+           <li><strong>Umesh Bhanot</strong> - Regional Head - South Asia, APL Logistics</li>
+           <li><strong>Ashish Bhatt</strong> - Managing Director, APL Logistics VASCOR Automotive Pvt. Ltd.</li>
+         </ul>
+					</p>
+				</div>
+				<div className='w-full h-full bg-red-50'>
+					<img
+						src=''
+						alt=''
+					/>  
+				</div>  
+			</div>
+      <div className='grid grid-cols-2 w-full h-dvh'>
+				<div className='px-6 py-16 order-2'>
+					<h3 className='text-primary font-bold inline-flex items-center justify-start gap-4 text-lg'>
+						{' '}
+						<span className='w-8 h-0.5 bg-primary inline-block' /> About Us
+					</h3>
+					<h2 className='text-5xl font-bold mb-4'>Management Team</h2>
+          <ul className="list-inside list-disc space-y-4 text-lg text-gray-700">
+           <li><strong>Ashish Bhatt</strong> - Managing Director, APL Logistics VASCOR Automotive Pvt. Ltd.</li>
+           <li><strong>Kunal Mahapatra</strong> - Sr. General Manager, APL Logistics VASCOR Automotive Pvt. Ltd.</li>
+           <li><strong>Malay Nandy</strong> - Sr. General Manager IT & Systems Development & Customer Service, APL Logistics VASCOR Automotive Pvt. Ltd.</li>
+           <li><strong>Tarak Haldar</strong> - Sr. General Manager, Accounts, APL Logistics VASCOR Automotive Pvt. Ltd.</li>
+         </ul>
+					
+				</div>
+				<div className='w-full h-full bg-red-50'>
+					<img
+						src=''
+						alt=''
+					/>
+				</div>
+        </div>
+      
+      
     </section>
     
   );
