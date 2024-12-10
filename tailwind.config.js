d243bc74b97d53ba703c398b20800ca7ed8de2a6
thegrimkiller22 <anubhav44r@gmail.com>
2024-12-10 11:35:57 +0530

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
-	darkMode: ["class"],
-	content: [
-	  "./src/**/*.{html,js,jsx,ts,tsx}",
-	],
-	theme: {
-	  extend: {
-		borderRadius: {
-		  lg: 'var(--radius)',
-		  md: 'calc(var(--radius) - 2px)',
-		  sm: 'calc(var(--radius) - 4px)',
-		},
-		colors: {},
-	  },
-	},
-	plugins: [require("tailwindcss-animate")],
-  };
+  darkMode: ["class"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+    },
+  },
+  plugins: [tailwindcssAnimate],
+};
