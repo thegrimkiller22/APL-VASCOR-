b022aeb8a05353188ed0b2ccc32150cd82f39380
thegrimkiller22 <anubhav44r@gmail.com>
2024-12-10 11:29:06 +0530

    tailwind edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
-module.exports = {
-    darkMode: ["class"],
-    content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-  ],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-}
+export default {
+	darkMode: ["class"],
+	content: [
+	  "./src/**/*.{html,js,jsx,ts,tsx}",
+	],
+	theme: {
+	  extend: {
+		borderRadius: {
+		  lg: 'var(--radius)',
+		  md: 'calc(var(--radius) - 2px)',
+		  sm: 'calc(var(--radius) - 4px)',
+		},
+		colors: {},
+	  },
+	},
+	plugins: [require("tailwindcss-animate")],
+  };
